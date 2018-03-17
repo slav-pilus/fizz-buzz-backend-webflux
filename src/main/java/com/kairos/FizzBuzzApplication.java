b5f6aa81a519dfee69430587f1c2f5018b9a1b26
slavP slav.pilus@gmail.com <slav.pilus@gmail.com>
2018-03-17 13:42:49 +0000

    Project Initialized

---
+++ src/main/java/com/kairos/FizzBuzzApplication.java
@@ -0,0 +1,12 @@
+package com.kairos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzzApplication.class, args);
+	}
+}
