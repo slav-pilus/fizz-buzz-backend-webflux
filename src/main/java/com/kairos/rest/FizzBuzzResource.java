cfe19681c8841fe460e44041599d769d9a85b826
slavP <slav.pilus@gmail.com>
2018-03-18 14:23:56 +0000

    setting cors

--- src/main/java/com/kairos/rest/FizzBuzzResource.java
+++ src/main/java/com/kairos/rest/FizzBuzzResource.java
@@ -4,11 +4,13 @@ import com.kairos.rest.exception.FizzBuzzException;
 import com.kairos.rest.vm.FizzBuzzVM;
 import com.kairos.service.FizzBuzzService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "${settings.cors-origin}")
 public class FizzBuzzResource {
 
     @Autowired
